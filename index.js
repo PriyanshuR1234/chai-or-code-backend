605a9fc76d9acd38019cccc19a9a8f406f7e5006
Priyanshu Rajput <priyanshurajput1386@gmail.com>
2025-01-30 15:56:37 +0530

    hello this change

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const githubdata={
+    "login": "PriyanshuR1234",
+    "id": 140266349,
+    "node_id": "U_kgDOCFxLbQ",
+    "avatar_url": "https://avatars.githubusercontent.com/u/140266349?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/PriyanshuR1234",
+    "html_url": "https://github.com/PriyanshuR1234",
+    "followers_url": "https://api.github.com/users/PriyanshuR1234/followers",
+    "following_url": "https://api.github.com/users/PriyanshuR1234/following{/other_user}",
+    "gists_url": "https://api.github.com/users/PriyanshuR1234/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/PriyanshuR1234/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/PriyanshuR1234/subscriptions",
+    "organizations_url": "https://api.github.com/users/PriyanshuR1234/orgs",
+    "repos_url": "https://api.github.com/users/PriyanshuR1234/repos",
+    "events_url": "https://api.github.com/users/PriyanshuR1234/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/PriyanshuR1234/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "Priyanshu Rajput",
+    "company": null,
+    "blog": "",
+    "location": "INDIA UP",
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 16,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2023-07-22T16:11:26Z",
+    "updated_at": "2025-01-25T09:24:04Z"
+  }
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,6 +49,9 @@ app.get('/login',(req,res)=>{
 })
 app.get('/youtube',(req,res)=>{
     res.send("<h2>Chai aur code</h2>")
+})
+app.get('github',(req,res)=>{
+    res.json(githubdata)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
